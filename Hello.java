7960462794f73783319bfe04013817383a1f5718
kshashank03 <shashankkalanithi@gmail.com>
2022-08-11 16:50:44 -0700

    First commit

---
+++ Hello.java
@@ -0,0 +1,12 @@
+// Every Java Program must define a class and all code is inside a class
+// Everything in Java must hae a type
+// Every Java program must have a function called public static void main(String[] args)
+public class Hello{
+    // public = this method can be called by anyone
+    // static = this is a method of the class, but not any specific instance of the class
+    // void = the method 'main' will not return a value
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ StringMethods.java
@@ -0,0 +1,9 @@
+public class StringMethods{
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(s);
+        System.out.println("Pull the 3rd char: " + s.charAt(3));
+        System.out.println("Pull the 2nd to the 4th character: " + s.substring(2, 4));
+        System.out.
+    }
+}
